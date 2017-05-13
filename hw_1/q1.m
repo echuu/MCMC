66c370544356839b5f432616989579ed93f665fb
echuu <chuu3ric@gmail.com>
2017-05-12 20:01:24 -0700

    proj2 start

--- hw_1/q1.m
+++ hw_1/q1.m
@@ -28,11 +28,23 @@ K_2 = [  0    0    0  0.4   0.6;
 left_evec = V0(:,1);
 norm_left = abs(left_evec / norm(left_evec, 1))'; % left eigen-vector
 
+csvwrite('evec0.csv', round(V0, 3));
+csvwrite('evec1.csv', round(V1, 3));
+csvwrite('evec2.csv', round(V2, 3));
 
 %% part 3. how many, what are the invariant probabilities?
 %  axis([-1 1 -1 1])
 %  scatterplot(lambda_0)
 
+% invariant probabilities
+
+v0 = V0(:,1) / norm(V0(:,1), 1);
+v1 = V1(:,1) / norm(V1(:,1), 1);
+v2 = V2(:,1) / norm(V2(:,1), 1);
+
+csvwrite('left_0.csv', -v0');
+csvwrite('left_1.csv', -v1');
+csvwrite('left_2.csv', -v2');
 
 %% part 4. plot eigenvalues as dots on 2-D plane (inside unit circle)
 K_01 = K_0^50;
@@ -43,10 +55,15 @@ K_02 = K_0^200;
 
 
 
+
 %% plot D0, lambda_01, lambda_02 on 2-D plane
-lambda_0  = sort(diag(D0),  'descend');
-lambda_01 = sort(diag(D01), 'descend');
-lambda_02 = sort(diag(D02), 'descend');
+lambda_0  = round(diag(D0), 3);
+lambda_01 = round(diag(D1), 3);
+lambda_02 = round(diag(D2), 3);
+
+csvwrite('eval0.csv', round(lambda_0,  3));
+csvwrite('eval1.csv', round(lambda_01, 3));
+csvwrite('eval2.csv', round(lambda_02, 3));
 
 
 re = [real(lambda_0); real(lambda_01); real(lambda_02)];
