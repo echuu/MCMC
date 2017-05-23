ec0435c936a904da72736938497e79c43a7e4d44
echuu <chuu3ric@gmail.com>
2017-05-23 00:45:50 -0700

    proj2 in C skeleton

---
+++ proj_3/clusterSampling.c
@@ -0,0 +1,36 @@
+/** clusterSampling.c
+
+STATS 202C: PROJECT 3
+Cluster Sampling of the Ising/Potts Model
+
+Author: Eric Chuu
+
+**/
+
+#include<stdio.h>
+#include<string.h>
+
+
+/** functions to implement:
+	
+	getNeighborSpins: calcualte energy at position (r,c)
+	updateState: based on random number in [0,1] and conditional probs
+	p: calculate the probability, metropolis ratio
+	gibbs: main function that runs the gibbs sampler
+
+**/
+
+
+
+int main() {
+
+
+
+	printf("STATS 202C: PROJECT 3\n");
+}
+
+
+
+
+
+
