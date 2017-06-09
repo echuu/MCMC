780c11b2977ad60d2a922347ebe466d95e8bce93
echuu <chuu3ric@gmail.com>
2017-06-09 12:57:14 -0700

    start hw2

---
+++ hw_2/hwCode.R
@@ -0,0 +1,32 @@
+
+# STATS 202C: HW 2
+
+
+
+
+
+
+# Problem 3 -- Shuffling Cards
+
+X_0 = c(1:52);     # deck of cards
+
+# simulate shuffling process iteratively from X_{t-1} to X_{t}
+
+
+
+# shuffling function, takes input of current state
+# returns next iteration (one iteration of shuffling)
+shuffle = function(X_cur) {
+    
+}
+
+# measure of randomness as function of time
+err = function(t) {
+    
+    
+    
+}
+
+
+
+
